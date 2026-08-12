e6a6918faa8ffc888ee7ce0c698ebad7c93ea6c4
jdongdong <dongdol0121@gmail.com>
2026-08-12 23:29:36 +0900

    테스트 2차

--- backend/src/main/java/com/sejong/sjc_app/SjcAppApplication.java
+++ backend/src/main/java/com/sejong/sjc_app/SjcAppApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SjcAppApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SjcAppApplication.class, args);
 	}
 
