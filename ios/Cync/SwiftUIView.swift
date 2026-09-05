5b543adb942a0ca30c54548b2082012badc0db5f
도하윤 <aox1ks@gmail.com>
2026-09-05 16:10:00 +0900

    클로드 프론트 md 사물함 UI(보완필요) community view 수정 게시물 댓글 수정중 (keyboard가 화면에 안 뜸 오류 확인 필요)

---
+++ ios/Cync/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Cync
+//
+//  Created by 도하윤 on 9/5/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
